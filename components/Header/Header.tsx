3c15fd4f431bbce2bacfe51aac4003481dc1c0f1
Buzuev1234 <buzuevaleksandr71@gmail.coom>
2024-03-12 04:57:45 +1000

    update logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,8 +9,8 @@ const Header = () => (
       <div className="cursor-pointer">
         <Media
           type="image"
-          link="/images/Header/music-medium-logo.svg"
-          blurLink="/images/Header/music-medium-logo.png"
+          link="/images/logo_big.png"
+          blurLink="/images/logo_short.png"
           containerClasses="md:w-[135px] md:h-[53px]
             xs:w-[100px] samsungS8:w-[96px] w-[85.3px]
             aspect-[135/53]"
@@ -18,19 +18,6 @@ const Header = () => (
       </div>
     </Link>
     <div className="flex gap-x-[10px]">
-      <Button
-        id="logo-btn"
-        type="button"
-        className="aspect-[210/40] !w-[179.2px] rounded-full bg-gray_overlay_6
-          bg-none font-urwgeometric_semibold text-[14px]
-          !text-[#a1ea04] shadow-[0px_12px_24px_0px_#0000003d]
-          backdrop-blur-[20px]
-          samsungS8:!w-[201.6px] xs:!w-[210px] xs:text-[16px]"
-        pulseColor="white"
-        bgVariant="primary"
-      >
-        <p className="drop-shadow-xl drop-shadow-session">Sound Studios</p>
-      </Button>
       <LogoutButton />
     </div>
   </>
