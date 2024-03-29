91fa6392efc6719f57417b6455758d836598bb87
Buzuev1234 <buzuevaleksandr71@gmail.com>
2024-03-29 17:42:31 +0100

    fixed

--- components/Pages/SignUpPage/SignUpPage.tsx
+++ components/Pages/SignUpPage/SignUpPage.tsx
@@ -1,6 +1,6 @@
 import EmailPasswordForm from "./EmailPasswordForm"
 
-const SignUpPage = () => {
+const SignUpPage = () => 
   return (
     <div
       className="no-scrollbar relative flex h-screen w-screen bg-center bg-cover items-center justify-center"
@@ -9,6 +9,5 @@ const SignUpPage = () => {
       <EmailPasswordForm />
     </div>
   )
-}
 
 export default SignUpPage
