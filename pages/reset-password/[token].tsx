00128bcb87b42d13dcf5a26dd42c47bb6fa84d82
jacob143590 <jacob143590@gmail.com>
2024-05-17 23:35:14 +0800

    updated

--- pages/reset-password/[token].tsx
+++ pages/reset-password/[token].tsx
@@ -3,15 +3,6 @@ import ResetPassword from '@/components/Pages/ResetPassword';
 
 // import { useRouter } from 'next/router';
 
-const ResetPass = () => {
-    // const router = useRouter();
-    // const { token } = router.query;
-
-   
-
-    return (
-        <ResetPassword />
-    );
-};
+const ResetPass = () => <ResetPassword />;
 
 export default ResetPass;
