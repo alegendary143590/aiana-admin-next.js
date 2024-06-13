01e3d07652f8f4acb628a9205d69452053d0a440
jacob143590 <jacob143590@gmail.com>
2024-06-14 01:45:38 +0800

    updated

--- components/Pages/KnowledgeBasePage/Text.tsx
+++ components/Pages/KnowledgeBasePage/Text.tsx
@@ -34,7 +34,7 @@ const Text = ({questionAnswers, setQuestionAnswers}) => {
   }
 
   const handleDeleteQA = (index) => {
-    const updatedQuestionAnswers = questionAnswers.filter((qa, i) => i !== index)
+    const updatedQuestionAnswers = questionAnswers.filter((_, i) => i !== index)
     setQuestionAnswers(updatedQuestionAnswers)
   }
 
