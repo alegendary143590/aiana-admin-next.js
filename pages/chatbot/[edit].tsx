97d45c49dd2688e08f338d61eec2f63fc781f883
jacob143590 <jacob143590@gmail.com>
2024-06-16 22:42:35 +0800

    fixed kan 28

--- pages/chatbot/[edit].tsx
+++ pages/chatbot/[edit].tsx
@@ -1,15 +1,13 @@
-import { useRouter } from "next/router"
+
 import ChatbotForm from "@/components/ChatbotFrom"
 import Layout from "@/components/Layout"
 
 const EditPage = () => {
-  const router = useRouter()
-  const { bot } = router.query;
 
   return (
     <Layout type="admin">
       <div className="px-[20px] py-[20px] w-full h-full flex flex-col">
-        <ChatbotForm bot={bot} />
+        <ChatbotForm />
       </div>
     </Layout>
   )
