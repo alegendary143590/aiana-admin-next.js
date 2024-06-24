27c1e5f0269bd0318ac15fbd6d0992f94b64036e
jacob143590 <jacob143590@gmail.com>
2024-06-24 14:32:01 +0800

    fixed kan 60

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,20 @@ import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
 import * as React from "react"
 import { Analytics } from "@vercel/analytics/react"
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+
 import TokenProvider from "@/providers/TokenContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <TokenProvider >
-      <Component {...pageProps} />
-      <ToastContainer />
-      <Analytics />
+      <ThemeProvider theme={theme} >
+        <Component {...pageProps} />
+        <ToastContainer />
+        <Analytics />
+      </ThemeProvider>
     </TokenProvider>
   )
 }
