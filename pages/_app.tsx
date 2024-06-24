2bd99442b666f7a69053704777261f8e5c866f16
jacob143590 <jacob143590@gmail.com>
2024-06-24 14:54:45 +0800

    updated kan 60

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,18 @@ import "@amir04lm26/react-modern-calendar-date-picker/lib/DatePicker.css"
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
 import * as React from "react"
+import { Analytics } from "@vercel/analytics/react"
+import TokenProvider from "@/providers/TokenContext"
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <TokenProvider >
         <Component {...pageProps} />
         <ToastContainer />
-    </>
-        
+        <Analytics />
+    </TokenProvider>
   )
 }
 export default MyApp
