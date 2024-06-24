3aa781577134309fbb873a44c6c4385ca5b2f8df
jacob143590 <jacob143590@gmail.com>
2024-06-24 14:34:28 +0800

    fixed error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import "../styles/globals.css"
 import "react-toastify/dist/ReactToastify.css"
 import "@amir04lm26/react-modern-calendar-date-picker/lib/DatePicker.css"
-
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
 import * as React from "react"
 import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 
 import TokenProvider from "@/providers/TokenContext"
