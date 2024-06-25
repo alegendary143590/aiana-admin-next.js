ff660287655f283c1f3834931e1ddcf14f41b556
jacob143590 <jacob143590@gmail.com>
2024-06-26 04:05:59 +0800

    updated

--- components/CodeSegment/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const CodeSegment = ({ content }) => 
-    <pre className='codeContainer'>
-      <code>{content}</code>
-    </pre>
-
-export default CodeSegment;
--- components/TextSegment/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TextSegment = ({ content }) => {
-  return <p>{content}</p>;
-};
-
-export default TextSegment;
