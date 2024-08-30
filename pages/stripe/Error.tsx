02e88dda37c04b9e30a9eba3bad457b26f05da2a
jacob143590 <jacob143590@gmail.com>
2024-08-30 17:58:58 +0800

    add stripe function

---
+++ pages/stripe/Error.tsx
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+        <div className="container">
+            <h1>Payment Failed</h1>
+            <p>Sorry, there was an issue processing your payment. Please try again.</p>
+        </div>
+    );
+}
---
+++ pages/stripe/index.tsx
@@ -0,0 +1,11 @@
+import CheckoutButton from "@/components/CheckoutButton";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Sample Product</h1>
+      <p>Price: $10.00</p>
+      <CheckoutButton />
+    </div>
+  );
+}
