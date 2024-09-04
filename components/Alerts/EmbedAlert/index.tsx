23cb796426fd290d78b0dc7b5c89b5102a604e47
jacob143590 <jacob143590@gmail.com>
2024-09-04 17:26:41 +0800

    updated for adding domain

--- components/Alerts/EmbedAlert/index.tsx
+++ components/Alerts/EmbedAlert/index.tsx
@@ -4,7 +4,6 @@ import React from "react"
 export default function EmbedAlert({ open, setOpen, description, handleCopy }) {
   const t = useTranslations('common')
   const alertRef = React.useRef(null)
-  const [domainInput, setDomainInput] = React.useState("");
   React.useEffect(() => {
     const handleClickOutside = (event) => {
       if (alertRef.current && !alertRef.current.contains(event.target)) {
