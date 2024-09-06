efde1294db0d04e090bd5ea1031ab942c6f4c431
jacob143590 <jacob143590@gmail.com>
2024-09-06 17:54:44 +0800

    fixed vercel error

--- components/Pages/PricingPage/PricingPage.tsx
+++ components/Pages/PricingPage/PricingPage.tsx
@@ -2,12 +2,11 @@
 import PricingTable from "@/components/PricingTable"
 import Layout from "../../Layout"
 
-const PricingPage = () => (
+const PricingPage = () => 
   <Layout type="admin">
     <div className="px-[20px] py-[20px] w-full h-full flex flex-col">
       <PricingTable />
     </div>
   </Layout>
-)
 
 export default PricingPage
